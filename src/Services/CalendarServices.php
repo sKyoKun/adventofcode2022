61cd37eb947c19f34a8f7b1421ebead62f8278ae
sKyoKun <sk.kyokun@gmail.com>
2021-12-07 10:27:33 +0100

    [DAY 7] Whales attack

--- src/Services/CalendarServices.php
+++ src/Services/CalendarServices.php
@@ -5,5 +5,17 @@ namespace App\Services;
 
 class CalendarServices
 {
+    public function parseInputWithIntegersAndComma(array $lines)
+    {
+        $finalArray = [];
+        foreach ($lines as $key => $line) {
+            $arrLine = explode(',', $line);
+            foreach ($arrLine as $keyLine => $number) {
+                $arrLine[$keyLine] = (int)$number;
+            }
+            $finalArray[$key] = $arrLine;
+        }
 
+        return $finalArray;
+    }
 }
